7e640f36da94fb6fc9eddb89ba0200c7faf2a75f
eronryabets <eronryabets@gmail.com>
2024-06-18 17:07:21 +0300

    env

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,11 +1,13 @@
-
-
-class MarvelService {
-
+import { Component } from 'react';
+class MarvelService extends Component {
+    
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=5e2e281dbe74a7071aeffb1e8cfda4f6';
-     getResource = async (url) => {
-        let res = await fetch(url);
+    // _apiKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY;
+    _apiKey = process.env.REACT_APP_MARVEL_PUBLIC_API_KEY;
+
+
+    getResource = async (url) => {
+        const res = await fetch(url);
 
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status ${res.status}`);
@@ -15,13 +17,12 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-         return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&apikey=${this._apiKey}`);
     }
 
     getCharacter = (id) => {
-         return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this.getResource(`${this._apiBase}characters/${id}?apikey=${this._apiKey}`);
     }
-
 }
 
-export  default MarvelService;
+export default MarvelService;
