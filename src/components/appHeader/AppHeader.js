f3950a590b13d9d06a84a2d437edbaaa45f3f0df
eronryabets <eronryabets@gmail.com>
2024-07-24 11:23:10 +0300

    comic page

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -18,7 +18,6 @@ const AppHeader = () => {
                         to="/characters">Characters</NavLink></li>
                     /
                     <li><NavLink
-                        end
                         style={({isActive}) =>
                             ({color: isActive ? '#9f0013' : 'inherit'})}
                         to="/comics">Comics</NavLink></li>
