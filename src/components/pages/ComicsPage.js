74702309cb59a436d2bbbb385efad997b0fe75ac
eronryabets <eronryabets@gmail.com>
2024-08-03 17:40:18 +0300

    React Helmet

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,10 +1,18 @@
 import AppBanner from "../appBanner/AppBanner";
 import ComicsList from "../comicsList/ComicsList";
+import {Helmet} from "react-helmet";
 
 const ComicsPage = () => {
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list comics"
+                />
+                <title>Comics</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>
         </>
--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,62 +1,66 @@
-import {useParams, Link} from "react-router-dom";
-import {useState, useEffect} from 'react';
+import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 import useMarvelService from '../../services/UseMarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import './SingleComicPage.scss';
-import Skeleton from "../skeleton/Skeleton";
+import { Helmet } from "react-helmet";
 
 const SingleComicPage = () => {
-
-    const {comicId} = useParams();
+    const { comicId } = useParams();
     const [comic, setComic] = useState(null);
-    const {loading, error, getComic, clearError} = useMarvelService();
+    const { loading, error, getComic, clearError } = useMarvelService();
 
     useEffect(() => {
-        updateComic()
-        // eslint-disable-next-line
-    }, [comicId])
+        updateComic();
+    }, [comicId]);
 
     const updateComic = () => {
         clearError();
-        getComic(comicId)
-            .then(onComicLoaded)
-    }
+        getComic(comicId).then(onComicLoaded);
+    };
 
     const onComicLoaded = (comic) => {
         setComic(comic);
-    }
+    };
 
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error || !comic) ? <View comic={comic}/> : null;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !comic) ? <View comic={comic} /> : null;
 
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list comics"
+                />
+                <title>{comic ? comic.title : 'Comic'}</title>
+            </Helmet>
             {errorMessage}
             {spinner}
             {content}
         </>
-    )
-}
+    );
+};
 
-const View = ({comic}) => {
-    const {title, description, pageCount, thumbnail, language, price} = comic;
+const View = ({ comic }) => {
+    const { title, description, pageCount, thumbnail, language, price } = comic;
 
     return (
         <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
+            <img src={thumbnail} alt={title} className="single-comic__img" />
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
                 <p className="single-comic__descr">{description}</p>
-                <p className="single-comic__descr">{pageCount}</p>
+                <p className="single-comic__descr">{pageCount} pages</p>
                 <p className="single-comic__descr">Language: {language}</p>
                 <div className="single-comic__price">{price}</div>
             </div>
             <Link to="/comics" className="single-comic__back">Back to all</Link>
         </div>
-    )
-}
+    );
+};
 
 export default SingleComicPage;
